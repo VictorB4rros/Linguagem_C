c0ed7befae58042389f87faf0c85808688cb6c28
Victor Barros <rodrigues.barros@aluno.ifsp.edu.br>
2024-11-26 17:27:49 -0300

    Algoritmos de ordenação

--- mergeSort.c
+++ mergeSort.c
@@ -2,6 +2,8 @@
 
 // Bibliotecas
 #include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
 #include "ordenacao.h"
 
 // Corpo do programa
@@ -16,14 +18,15 @@ int main(void) {
         printf ("Determine o tamanho do vetor que deseja ordenar: ");
         retorno = scanf ( "%d", &n );
         if ( retorno != 1) {
-            printf ("Entrada inválida! Por favor, tente novamente.");
+            printf ("Entrada inv%clida! Por favor, tente novamente.", 160);
             while (getchar() != '\n');
+            getch();
         }    
     } while ( retorno != 1 );
 
     int * vetor = (int *) malloc (n * sizeof(int));
     if (vetor == NULL) {
-        printf ("Erro ao alocar memória!!!\n");
+        printf ("Erro ao alocar mem%cria!!!\n", 162);
         return (1);
     }
 
@@ -33,28 +36,36 @@ int main(void) {
         system ("clear");
     #endif
 
-
-    printf ("Insira os números que serão ordenados: \n");
+    printf ("Insira os n%cmeros que ser%co ordenados: \n", 163, 198);
     for (int i = 0; i < n; i++) {
         do {
             printf("Elemento [%d]=> ", i);
             retorno = scanf ("%d", &vetor[i]);
             if ( retorno != 1) {
-                printf ("Entrada inválida! Por favor, tente novamente.");
+                printf ("Entrada inv%clida! Por favor, tente novamente.", 160);
                 while (getchar() != '\n');
+                getch ();
             }    
         } while ( retorno != 1 );
     }
 
     mergeSort (vetor, 0, n-1);
-    system ("cls");
+    
+    #ifdef _WIN32
+        system ("cls");
+    #else
+        system ("clear");
+    #endif
+
     printf ("Vetor ordenado: \n");
 
     for (int i = 0; i < n; i++) {
         printf (" %d", vetor[i]);
     }
 
-    getchar();
     free (vetor);
+    vetor = NULL;
+
+    getch();
     return (0);
 }
--- selectionSort.c
+++ selectionSort.c
@@ -2,31 +2,69 @@
 
 // Bibliotecas
 #include <stdio.h>
+#include <stdlib.h>
 #include <conio.h>
-#include <windows.h>
 #include "ordenacao.h"
 
 // Corpo do programa
 int main(void) {
-    int n;
-    printf ("Determine o tamanho do vetor que deseja ordenar: ");
-    scanf ( "%d", &n );
-    int vetor [n];
-    system ( "cls" );
+    int n, retorno;
+    do {
+        #ifdef _WIN32
+            system ("cls");
+        #else
+            system ("clear");
+        #endif
+        printf ("Determine o tamanho do vetor que deseja ordenar: ");
+        retorno = scanf ( "%d", &n );
+        if ( retorno != 1) {
+            printf ("Entrada inv%clida! Por favor, tente novamente.", 160);
+            while (getchar() != '\n');
+            getch();
+        }    
+    } while ( retorno != 1 );
+
+    int * vetor = (int *) malloc (n * sizeof(int));
+    if (vetor == NULL) {
+        printf ("Erro ao alocar mem%cria!!!\n", 162);
+        return (1);
+    }
+
+    #ifdef _WIN32
+        system ("cls");
+    #else
+        system ("clear");
+    #endif
 
     printf ("Insira os n%cmeros que ser%co ordenados: \n", 163, 198);
     for (int i = 0; i < n; i++) {
-        printf("Elemento [%d]=> ", i);
-        scanf ("%d", &vetor[i]);
+        do {
+            printf("Elemento [%d]=> ", i);
+            retorno = scanf ("%d", &vetor[i]);
+            if ( retorno != 1) {
+                printf ("Entrada inv%clida! Por favor, tente novamente.", 160);
+                while (getchar() != '\n');
+                getch ();
+            }    
+        } while ( retorno != 1 );
     }
 
     selectionSort (vetor, n);
-    system ("cls");
+
+    #ifdef _WIN32
+        system ("cls");
+    #else
+        system ("clear");
+    #endif
+
     printf("Vetor ordenado: \n");
 
     for (int i = 0; i < n; i++) {
         printf (" %d", vetor[i]);
     }
+
+    free (vetor);
+    vetor = NULL;
 
     getch();
     return (0);
