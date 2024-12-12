9733a7b6df37018cce4bf4d7f4365d4f9908bfa8
Victor Barros <rodrigues.barros@aluno.ifsp.edu.br>
2024-12-12 14:56:07 -0300

    Algoritmos de ordenação

--- ordenacao.h
+++ ordenacao.h
@@ -1,5 +1,7 @@
 // Definições das funções de ordenação
 
+// Definição de Bubble Sort
+// n é o tamanho do vetor
 void bubbleSort (int vetor[], int n) {
     int i, j, auxiliar, troca;
     for (i = 1; i < n; i++) {
@@ -16,6 +18,8 @@ void bubbleSort (int vetor[], int n) {
     }
 }
 
+// Definição de Selection Sort
+// n é o tamanho do vetor
 void selectionSort (int vetor[], int n) {
     int i, j, minimo, auxiliar;
     for (i = 0; i < n-1; i++) {
@@ -30,6 +34,8 @@ void selectionSort (int vetor[], int n) {
     }
 }
 
+// Definição de Insertio Sort
+// n é o tamanho do vetor
 void insertionSort (int vetor[], int n) {
     int i, j, auxiliar;
     for (i = 1; i < n; i++) {
@@ -103,12 +109,14 @@ void mergeSort (int vet[], int inicio, int fim) {
     }
 }
 
+// A rotina troca será usada para o Quick Sort
 void troca (int * a, int * b) {
     int t = *a;
     *a = *b;
     *b = t;
 }
 
+// A rotina divisão será usada para o Quick Sort
 int divisao (int vet[], int baixo, int alto) {
     // Escolha do pivô
     int pivot = vet[alto];
@@ -126,11 +134,58 @@ int divisao (int vet[], int baixo, int alto) {
     return (i+1);
 }
 
+// Baixo é o primeiro elemento do vetor e alto é o último elemento do vetor
 void quickSort (int vet[], int baixo, int alto) {
     if (baixo < alto) {
         int posicaoDoPivot = divisao (vet, baixo, alto);
 
         quickSort (vet, baixo, posicaoDoPivot - 1);
         quickSort (vet, posicaoDoPivot + 1, alto);
+    }
+}
+
+// n é o tamanho do vetor e i é o índice da raíz do heap
+void sift (int vet[], int n, int i) {
+    // inicializa o maior elemento como raíz da árvore
+    int maior = i;
+
+    int filhoEsquerda = 2 * i + 1;
+    int filhoDireita = 2 * i + 2;
+
+    // Se o filho da esquerda for maior do que a raíz, o índice dele é selecionado como maior
+    if (filhoEsquerda < n && vet[filhoEsquerda] > vet[maior]) 
+    maior = filhoEsquerda;
+
+    // Se o filho da direita for maior do que o maior até agora, o índice dele é selecionado como maior
+    if (filhoDireita < n && vet[filhoDireita] > vet[maior])
+    maior = filhoDireita;
+
+    // Se o índice do maior for diferente do índice da raíz, executa a troca
+    if (maior != i) {
+        int temp = vet[i];
+        vet[i] = vet[maior];
+        vet[maior] = temp;
+
+        // recursivamente realiza o sift na sub árvore afetada
+        sift (vet, n, maior);
+    }
+}
+
+void heapSort (int vet [], int n) {
+    // Construindo o heap
+    for (int i = n / 2 - 1; i >= 0; i--) {
+        sift (vet, n, i);
+    }
+
+    // Extrai um elemento do heap a cada iteração
+    for (int i = n - 1; i > 0; i--) {
+
+        // Move a raíz atual para o final
+        int temp = vet[0];
+        vet[0] = vet[i];
+        vet[i] = temp;
+
+        // Sift é chamado para arrumar o heap reduzido
+        sift (vet, i, 0);
     }
 }
